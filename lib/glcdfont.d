76f2790b32fc62cb63617535aa3bc1d0ca83f6b3
Aganchiran <aganchiran@gmail.com>
2020-10-24 17:19:59 +0200

    Corne-ble: First compile

---
+++ lib/glcdfont.d
@@ -0,0 +1,10 @@
+.build/obj_corne_ble_slave_default/../../lib/glcdfont.o: \
+ keyboards/corne_ble/keymaps/default/../../lib/glcdfont.c \
+ keyboards/corne_ble/config.h keyboards/corne_ble/slave/config.h \
+ keyboards/corne_ble/board/custom_board.h
+
+keyboards/corne_ble/config.h:
+
+keyboards/corne_ble/slave/config.h:
+
+keyboards/corne_ble/board/custom_board.h:
