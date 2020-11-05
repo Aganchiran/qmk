edfb0ec9cdd6fb37d9372dbbc0d9779c3a3ade28
Aganchiran <aganchiran@gmail.com>
2020-11-05 18:19:45 +0100

    Corne-ble: Rearrange keymap

--- keyboards/corne_ble/keymaps/default/keymap.c
+++ keyboards/corne_ble/keymaps/default/keymap.c
@@ -38,6 +38,7 @@ enum custom_keycodes {
     RAISE,
     POWER_TAB,
     TOGGLE_MODS,
+    QUOTES,
     ADJUST
 };
 
@@ -63,41 +64,43 @@ typedef struct {
 ////////////////
 
 // Fillers to make layering more clear
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
+#define _______     KC_TRNS
+#define XXXXXXX     KC_NO
+#define RAISE_ENT   LT(_RAISE, KC_ENT)
+#define LOWER       MO(_LOWER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(\
 		KC_ESC,		KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,						KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		KC_TAB, \
-		KC_LSFT,	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,						KC_H,		KC_J,		KC_K,		KC_L,		KC_QUOT,	RSFT_T(KC_CAPS), \
+		KC_LSFT,	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,						KC_H,		KC_J,		KC_K,		KC_L,		QUOTES,	    RSFT_T(KC_CAPS), \
 		KC_LCTL,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,						KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SCLN,	KC_LALT, \
-											TD(FL_WIN),	KC_SPC,		LT(_RAISE, KC_ENT),			KC_RALT,	KC_BSPC,	MO(_LOWER)\
+											TD(FL_WIN), KC_SPC,     RAISE_ENT,			        KC_RALT,	KC_BSPC,	LOWER\
 		),
 
 [_LOWER] = LAYOUT(\
 		_______,	KC_DQUO,	KC_LWIN,	KC_EXLM,	KC_QUES,	XXXXXXX,					KC_AMPR,	KC_PIPE,	KC_PSCR,	XXXXXXX,	KC_PERC,	_______, \
 		_______,	KC_AT,		KC_DLR,		KC_LPRN,	KC_RPRN,	XXXXXXX,					KC_HASH,	XXXXXXX,	XXXXXXX,	XXXXXXX,	KC_GRV,		_______, \
-		_______,	XXXXXXX,	KC_CIRC,	KC_LBRC,	KC_RBRC,	XXXXXXX,					XXXXXXX,	KC_MINS,	XXXXXXX,	XXXXXXX,	XXXXXXX,	TOGGLE_MODS, \
+		_______,	XXXXXXX,	KC_CIRC,	KC_LBRC,	KC_RBRC,	LSFT(KC_MINS),				XXXXXXX,	KC_MINS,	XXXXXXX,	XXXXXXX,	TOGGLE_MODS,_______, \
 											_______,	_______,	_______,					_______,	_______,	_______\
 		),
 
 [_RAISE] = LAYOUT(\
-		_______,	KC_PSLS,	KC_7,		KC_8,		KC_9,		KC_PAST,					KC_BSLS,	XXXXXXX,	KC_P7,		KC_P8,		KC_P9,		_______, \
-		_______,	KC_PMNS,	KC_4,		KC_5,		KC_6,		KC_PPLS,					XXXXXXX,	XXXXXXX,	KC_P4,		KC_P5,		KC_P6,		_______, \
-		_______,	KC_EQL,		KC_1,		KC_2,		KC_3,		KC_0,						XXXXXXX,	KC_P0,		KC_P1,		KC_P2,		KC_P3,		_______, \
-											_______,	_______,	_______,					_______,	KC_DEL,		_______\
-		),
-
-[_FL] = LAYOUT(\
-		POWER_TAB,	XXXXXXX,	KC_HOME,	KC_UP,		KC_END,		XXXXXXX,					KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6, \
-		_______,	XXXXXXX,	KC_LEFT,	KC_DOWN,	KC_RGHT,	XXXXXXX,					KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12, \
-		_______,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,					XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	_______, \
+		_______,	XXXXXXX,	KC_HOME,	KC_UP,		KC_END,		XXXXXXX,					XXXXXXX,	KC_HOME,	KC_UP,		KC_END,		XXXXXXX,	_______, \
+		_______,	XXXXXXX,	KC_LEFT,	KC_DOWN,	KC_RGHT,	XXXXXXX,					XXXXXXX,	KC_LEFT,	KC_DOWN,	KC_RGHT,	XXXXXXX,	_______, \
+		_______,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	KC_BSPC,					XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	_______, \
 											_______,	_______,	_______,					_______,	_______,	_______\
 		),
 
+[_FL] = LAYOUT(\
+		POWER_TAB,	KC_7,		KC_8,		KC_9,		KC_PAST,	KC_PSLS,					KC_BSLS,	KC_F10,     KC_F7,		KC_F8,		KC_F9,		_______, \
+		_______,	KC_4,		KC_5,		KC_6,		KC_PPLS,	KC_PMNS,					XXXXXXX,	KC_F11,     KC_F4,		KC_F5,		KC_F6,		_______, \
+		_______,	KC_1,		KC_2,		KC_3,		KC_0,		KC_EQL,						XXXXXXX,	KC_F12,		KC_F1,		KC_F2,		KC_F3,		_______, \
+											_______,	_______,	_______,					_______,	KC_DEL,		_______\
+		),
+
 [_ADJUST] = LAYOUT(\
-		RGB_TOG,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,					RGB_M_P,	RGB_M_B,	RGB_M_R,	RGB_M_SW,	RGB_M_SN,	_______, \
+		_______,	RGB_TOG,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,					RGB_M_P,	RGB_M_B,	RGB_M_R,	RGB_M_SW,	RGB_M_SN,	_______, \
 		_______,	RGB_MOD,	RGB_SAI,	RGB_HUI,	XXXXXXX,	RGB_VAI,					RGB_M_K,	RGB_M_X,	RGB_M_G,	RGB_M_T,	XXXXXXX,	_______, \
 		_______,	RGB_RMOD,	RGB_SAD,	RGB_HUD,	XXXXXXX,	RGB_VAD,					XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	ENT_SLP,	_______, \
 		                                    _______,	_______,	_______,					_______,	_______,	_______\
@@ -211,6 +214,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #endif
 
     switch (keycode) {
+
+        case QUOTES:
+            normal_key(KC_QUOTE, record);
+            normal_key(KC_SPC, record);
+            return false;
+            break;
 
         case POWER_TAB:
             if (record->event.pressed) {
